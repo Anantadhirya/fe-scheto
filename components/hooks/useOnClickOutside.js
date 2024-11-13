b9700f36ee9338e890a5903a2cb04b7e5a9e9990
Anantadhirya <anantadhirya@gmail.com>
2024-11-13 18:38:23 +0700

    feat: useOnClickOutside hook

---
+++ components/hooks/useOnClickOutside.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+export function useOnClickOutside(ref, callback) {
+  useEffect(() => {
+    const handleClick = (e) => {
+      if (ref.current && !ref.current.contains(e.target)) {
+        callback(e);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClick);
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  }, [ref, callback]);
+}
