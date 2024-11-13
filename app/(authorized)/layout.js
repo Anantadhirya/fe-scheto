354686e20083b4d7749139edb98169faa103f2e2
Anantadhirya <anantadhirya@gmail.com>
2024-11-13 18:38:49 +0700

    feat: navigation bar

---
+++ app/(authorized)/layout.js
@@ -0,0 +1,5 @@
+import { Navbar } from "@/components/elements/navbar";
+
+export default function NavbarLayout({ children }) {
+  return <Navbar>{children}</Navbar>;
+}
---
+++ components/elements/navbar/Navbar.js
@@ -0,0 +1,84 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useRef, useState } from "react";
+
+import {
+  BsPersonFill,
+  BsFillPeopleFill,
+  BsFillEnvelopeOpenFill,
+} from "react-icons/bs";
+import { BiLogOut } from "react-icons/bi";
+import { IoMdHome } from "react-icons/io";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { useOnClickOutside } from "@/components/hooks";
+
+export function Navbar({ children }) {
+  const routes = [
+    {
+      name: "Scheto",
+      href: "",
+      icon: (
+        <div className="relative aspect-[1/1] w-[5vw]">
+          <Image src="/icon.svg" alt="" fill />
+        </div>
+      ),
+    },
+    { name: "Profile", href: "/profile", icon: <BsPersonFill /> },
+    { name: "Home", href: "/", icon: <IoMdHome /> },
+    { name: "Group", href: "/group", icon: <BsFillPeopleFill /> },
+    { name: "Inbox", href: "/inbox", icon: <BsFillEnvelopeOpenFill /> },
+  ];
+  const pathname = usePathname();
+  const [hide, setHide] = useState(true);
+  const navbarRef = useRef();
+  useOnClickOutside(navbarRef, () => setHide(true));
+  return (
+    <div className="flex max-md:flex-col">
+      <div className="flex w-full items-center gap-[2%] bg-blue-100 px-4 py-3 text-blue-200 md:hidden">
+        <button onClick={() => setHide(!hide)} className="text-lg">
+          <RxHamburgerMenu />
+        </button>
+        <div className="text-lg font-bold text-blue-300">Scheto</div>
+      </div>
+      <nav
+        ref={navbarRef}
+        className={`flex min-h-screen min-w-[200px] shrink-0 flex-col items-end gap-[2vh] bg-blue-100 pb-[7vh] pt-[10px] transition-transform duration-300 max-md:fixed max-md:w-[60%] md:w-[8%] md:pt-[max(5px,2vh)] ${hide ? "max-md:translate-x-[-100%]" : "max-md:translate-x-0"}`}
+      >
+        <button
+          onClick={() => setHide(!hide)}
+          className="mb-4 mb-[2vh] h-6 self-start px-5 text-lg text-blue-200 md:hidden"
+        >
+          <RxHamburgerMenu />
+        </button>
+        {routes.map((route, idx) => {
+          const active =
+            idx !== 0 &&
+            (route.href === "/"
+              ? pathname === route.href
+              : pathname.startsWith(route.href));
+          return (
+            <Link
+              key={route.name}
+              href={route.href}
+              className={`flex items-center justify-center gap-2 rounded-l-full py-[max(5px,1vh)] text-lg outline-0 max-md:w-[95%] md:w-[87%] md:text-[3vw] ${idx === 0 ? "pt-0 max-md:hidden" : ""} ${active ? "bg-blue-200 text-white" : "text-blue-200"}`}
+              onClick={() => setHide(true)}
+            >
+              {route.icon}
+              <div className="text-sm md:hidden">{route.name}</div>
+            </Link>
+          );
+        })}
+        <div className="h-[5vh] grow" />
+        <button className="flex w-full items-center justify-center gap-2 self-center text-blue-200 outline-0 md:w-[5vw] md:rounded-full">
+          <div className="flex aspect-[1/1] w-8 items-center justify-center rounded-full bg-white pr-1 text-xl shadow-xl md:w-full md:pr-[5%] md:text-[2.7vw]">
+            <BiLogOut />
+          </div>
+          <div className="text-sm md:hidden">Logout</div>
+        </button>
+      </nav>
+      {children}
+    </div>
+  );
+}
