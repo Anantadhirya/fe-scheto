751faa8f6ab6ffd7e29a12a8e6f4a574175de14c
Anantadhirya <anantadhirya@gmail.com>
2024-11-13 11:46:52 +0700

    init: react-hot-toast

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 const roboto = Roboto({
   weight: ["100", "300", "400", "500", "700", "900"],
@@ -15,7 +16,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={roboto.className}>
+        <Toaster />
+        {children}
+      </body>
     </html>
   );
 }
