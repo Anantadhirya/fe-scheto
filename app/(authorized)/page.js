cf03ff60396b510f2eb961c2593c75b7b44aa158
Anantadhirya <anantadhirya@gmail.com>
2024-11-23 23:43:11 +0700

    fix: home page calendar responsiveness

--- app/(authorized)/page.js
+++ app/(authorized)/page.js
@@ -23,7 +23,7 @@ export default function Home() {
       className={page !== "calendar" ? "max-md:hidden" : ""}
     >
       {/* Top Menu */}
-      <div className="relative z-30 flex h-28 flex-none items-center justify-between px-10 py-6 shadow-md">
+      <div className="relative z-30 flex flex-none justify-between py-6 shadow-md max-lg:h-fit max-lg:flex-col-reverse max-lg:gap-2 max-md:px-[min(40px,7vw-16.4px)] md:px-10 lg:h-28 lg:items-center">
         {page === "add" ? (
           // Add Menu
           <button
@@ -38,15 +38,16 @@ export default function Home() {
         ) : (
           <>
             {/* Left Top Menu */}
-            <div className="flex items-center gap-5 self-end text-2xl font-semibold text-blue-200">
+            <div className="flex items-center gap-5 text-2xl font-semibold text-blue-200 md:self-start lg:self-end">
               {page === "calendar" ? (
                 // Calendar
                 <>
-                  <div className="w-[180px]">
+                  <div className="w-[180px] text-nowrap">
                     {format(selectedWeek.from, "MMMM yyyy")}
                   </div>
                   <div className="flex gap-1">
                     <button
+                      className="outline-0"
                       onClick={() =>
                         setSelectedWeek({
                           from: startOfWeek(addWeeks(selectedWeek.from, -1)),
@@ -57,7 +58,7 @@ export default function Home() {
                       <BsCaretLeftFill />
                     </button>
                     <button
-                      className="rounded-full bg-blue-200 px-3 py-0.5 text-sm text-white"
+                      className="rounded-full bg-blue-200 px-3 py-0.5 text-sm text-white outline-0"
                       onClick={() =>
                         setSelectedWeek({
                           from: startOfWeek(new Date()),
@@ -68,6 +69,7 @@ export default function Home() {
                       Today
                     </button>
                     <button
+                      className="outline-0"
                       onClick={() =>
                         setSelectedWeek({
                           from: startOfWeek(addWeeks(selectedWeek.from, 1)),
@@ -85,11 +87,11 @@ export default function Home() {
               )}
             </div>
             {/* Right Top Menu */}
-            <div className="flex flex-col gap-3 self-start">
+            <div className="flex flex-col gap-3 max-lg:self-end lg:self-start">
               <div className="flex rounded-full bg-purple-200/80 text-base text-white">
                 <button
                   className={twMerge(
-                    "rounded-full px-12 py-0.5 text-center",
+                    "rounded-full px-12 py-0.5 text-center outline-0",
                     page === "calendar" && "bg-blue-200",
                   )}
                   onClick={() => setPage("calendar")}
@@ -98,7 +100,7 @@ export default function Home() {
                 </button>
                 <button
                   className={twMerge(
-                    "rounded-full px-14 py-0.5 text-center",
+                    "rounded-full px-14 py-0.5 text-center outline-0",
                     page === "list" && "bg-blue-200",
                   )}
                   onClick={() => setPage("list")}
